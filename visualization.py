6391ae2d18a45070dd824fb4b83ae52ddef95223
yonatangideoni <yonatangideoni@cs.huji.ac.il>
2021-10-22 19:28:51 +0300

    Created visualization.py

---
+++ visualization.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from read_input import read_counts_file
+
+
+def visualize_counts_plot(data: pd.Series, x_tick_every=20):
+    ax = data.plot.bar(ylabel="Counts")
+    ax.set_xticklabels([tick if not i % x_tick_every else "" for i, tick in enumerate(ax.get_xticklabels())])
+
+
+if __name__ == '__main__':
+    visualize_counts_plot(read_counts_file("histogram.itx"))
+    plt.show()
