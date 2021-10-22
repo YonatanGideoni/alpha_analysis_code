ae65458ebbd53756b633215d13555ef9d7f94bcf
yonatangideoni <yonatangideoni@cs.huji.ac.il>
2021-10-22 19:32:31 +0300

    Made x_ticks not scrunch up

--- visualization.py
+++ visualization.py
@@ -4,8 +4,8 @@ import pandas as pd
 from read_input import read_counts_file
 
 
-def visualize_counts_plot(data: pd.Series, x_tick_every=20):
-    ax = data.plot.bar(ylabel="Counts")
+def visualize_counts_plot(data: pd.Series, x_tick_every=100):
+    ax = data.plot.bar(ylabel="Counts", xlabel="Channel")
     ax.set_xticklabels([tick if not i % x_tick_every else "" for i, tick in enumerate(ax.get_xticklabels())])
 
 
