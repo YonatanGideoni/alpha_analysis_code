e79d516264dee02700cf80fd28f2f032db120811
yonatangideoni <yonatangideoni@cs.huji.ac.il>
2021-10-22 19:38:05 +0300

    Added rolling average and legend

--- visualization.py
+++ visualization.py
@@ -4,9 +4,14 @@ import pandas as pd
 from read_input import read_counts_file
 
 
-def visualize_counts_plot(data: pd.Series, x_tick_every=100):
-    ax = data.plot.bar(ylabel="Counts", xlabel="Channel")
+def visualize_counts_plot(data: pd.Series, x_tick_every=100, rolling_avg_size=5):
+    ax = data.rolling(window=rolling_avg_size).mean().plot(c="k",
+                                                           label=f"Rolling Mean, Window Size: {rolling_avg_size}")
+    ax = data.plot.bar(ylabel="Counts", xlabel="Channel", label="Raw Data", ax=ax)
+
     ax.set_xticklabels([tick if not i % x_tick_every else "" for i, tick in enumerate(ax.get_xticklabels())])
+
+    plt.legend()
 
 
 if __name__ == '__main__':
