78abe9d931c5f984c8fab951a345357b7827533a
yonatangideoni <yonatangideoni@cs.huji.ac.il>
2021-10-22 19:32:16 +0300

    Added max_row param

--- read_input.py
+++ read_input.py
@@ -28,12 +28,14 @@ def itx_to_pandas(path: str) -> pd.DataFrame:
     return dfn
 
 
-def read_counts_file(path: str) -> pd.Series:
+def read_counts_file(path: str, max_row=2048) -> pd.Series:
     with open(path, "r") as f:
         raw_data = f.read().split("\n")
     numeric_data = [float(data) for data in raw_data if data.isnumeric()]
 
-    return pd.Series(numeric_data)
+    data_df = pd.Series(numeric_data)
+
+    return data_df.iloc[:max_row]
 
 
 if __name__ == '__main__':
