e87bfad80c7e0ae4903c6ee8f6b69187f3cb3667
yonatangideoni <yonatangideoni@cs.huji.ac.il>
2021-10-23 21:33:30 +0300

    Added normalize param

--- visualization.py
+++ visualization.py
@@ -4,10 +4,13 @@ import pandas as pd
 from read_input import read_counts_file
 
 
-def visualize_counts_plot(data: pd.Series, x_tick_every=100, rolling_avg_size=5):
+def visualize_counts_plot(data: pd.Series, x_tick_every=100, rolling_avg_size=5, normalize=True):
+    if normalize:
+        data /= data.sum()
+
     ax = data.rolling(window=rolling_avg_size).mean().plot(c="k",
                                                            label=f"Rolling Mean, Window Size: {rolling_avg_size}")
-    ax = data.plot.bar(ylabel="Counts", xlabel="Channel", label="Raw Data", ax=ax)
+    ax = data.plot.bar(ylabel="Density" if normalize else "Counts", xlabel="Channel", label="Raw Data", ax=ax)
 
     ax.set_xticklabels([tick if not i % x_tick_every else "" for i, tick in enumerate(ax.get_xticklabels())])
 
