ff69f44d123eb771364fa935f193ea88327685ec
yonatangideoni <yonatangideoni@cs.huji.ac.il>
2021-10-25 09:06:30 +0300

    Improved fit_gaussian_to_peak

--- peak_analysis.py
+++ peak_analysis.py
@@ -7,6 +7,7 @@ from scipy.optimize import curve_fit
 from scipy.stats import linregress
 
 from read_input import read_counts_file
+from visualization import plot_peak_info
 
 
 def find_peaks(data: pd.Series, max_rel_peak_size=5., min_peak_dist=15):
@@ -17,20 +18,30 @@ def find_peaks(data: pd.Series, max_rel_peak_size=5., min_peak_dist=15):
 
 
 def area_based_gaussian(x, I, s, u):
-    return (I / (2 * np.pi * s ** 2)) * np.exp((x - u) ** 2 / (2 * s ** 2))
+    return (I / (2 * np.pi * s ** 2)) * np.exp(-(x - u) ** 2 / (2 * s ** 2))
 
 
-def fit_gaussian_to_peak(data, peak_channel, delta=10, plot=False):
+def fit_gaussian_to_peak(data, peak_channel, delta=8, plot=False):
     channels = np.arange(peak_channel - delta, peak_channel + delta + 1)
     energy_spectrum = data.iloc[peak_channel - delta:peak_channel + delta + 1]
 
     area_init_guess = energy_spectrum.sum()
     params, cov_mat = curve_fit(area_based_gaussian, channels, energy_spectrum,
-                                p0=[area_init_guess, delta, peak_channel])
+                                p0=[area_init_guess, delta, peak_channel], bounds=[0, np.inf])
     I, s, u = params
 
     if plot:
-        plt.plot(channels, area_based_gaussian(channels, I, s, u))
+        colour = np.random.rand(3)
+        dense_channels = np.linspace(channels.min(), channels.max())
+        plt.plot(dense_channels, area_based_gaussian(dense_channels, I, s, u), linewidth=3, c=colour)
+
+        spread_channels = np.linspace(channels.min() - delta, channels.max() + delta)
+        plt.plot(spread_channels, area_based_gaussian(spread_channels, I, s, u), linewidth=2, c=colour,
+                 linestyle='dashed')
+
+        plot_peak_info(peak_channel, energy_spectrum.max(), I, s, u, *np.diagonal(cov_mat) ** 0.5)
+
+    return params, cov_mat
 
 
 if __name__ == '__main__':
