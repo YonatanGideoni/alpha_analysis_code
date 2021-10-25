e4fb3db65b3145999ba6b5a10a41ca6bbea84eb3
yonatangideoni <yonatangideoni@cs.huji.ac.il>
2021-10-25 09:02:44 +0300

    Created plot_peak_info

--- visualization.py
+++ visualization.py
@@ -1,9 +1,5 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-from scipy import signal
-
-from peak_analysis import find_peaks, fit_gaussian_to_peak
-from read_input import read_counts_file
 
 
 def visualize_counts_plot(data: pd.Series, x_tick_every=100, rolling_avg_size=5, normalize=True, plot_peaks=True):
@@ -15,6 +11,8 @@ def visualize_counts_plot(data: pd.Series, x_tick_every=100, rolling_avg_size=5,
     ax = data.plot.bar(ylabel="Density" if normalize else "Counts", xlabel="Channel", label="Raw Data", ax=ax, width=1.)
 
     if plot_peaks:
+        from peak_analysis import find_peaks
+
         peaks, peak_properties = find_peaks(data)
 
         ax = data.where(data.index.isin(peaks), 0).plot.bar(color='r', ax=ax, label="Peaks", width=1)
@@ -23,12 +21,8 @@ def visualize_counts_plot(data: pd.Series, x_tick_every=100, rolling_avg_size=5,
     plt.legend()
 
 
-if __name__ == '__main__':
-    data = read_counts_file("thr45measurement1050.itx")
-    visualize_counts_plot(data)
-
-    peaks, _ = find_peaks(data)
-    for peak in peaks:
-        fit_gaussian_to_peak(data, peak, plot=True)
-
-    plt.show()
+def plot_peak_info(peak_channel, peak_height, area, std, mean, area_std, std_std, mean_std, text_height_mul=1.05):
+    peak_text = f"$\mu$={mean:.1f}$\pm${mean_std:.1f}\n" \
+                f"$\sigma$={std:.2f}$\pm${std_std:.2f}\n" \
+                f"Area={area:.2f}$\pm${area_std:.2f}"
+    plt.annotate(peak_text, xy=(peak_channel, peak_height), ha='center')
