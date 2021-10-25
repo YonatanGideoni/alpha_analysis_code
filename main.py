c32b289dfffd74edfad82c93240e49b36ca86982
yonatangideoni <yonatangideoni@cs.huji.ac.il>
2021-10-25 09:26:57 +0300

    Collect peak location and std

--- main.py
+++ main.py
@@ -9,8 +9,12 @@ if __name__ == '__main__':
     visualize_counts_plot(data)
 
     peaks, _ = find_peaks(data)
+    peak_loc = []
+    peak_std = []
     for peak in peaks:
-        fit_gaussian_to_peak(data, peak, plot=True)
+        [peak_area, gaussian_std, peak_channel], cov_mat = fit_gaussian_to_peak(data, peak, plot=True)
+        peak_loc.append(peak_channel)
+        peak_std.append(cov_mat[-1, -1] ** 0.5)
 
     plt.xlim(min(peaks) * 0.8)
 
