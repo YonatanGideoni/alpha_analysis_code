4ae89ddab9d31d92e9b66c506451643b771874f6
yonatangideoni <yonatangideoni@cs.huji.ac.il>
2021-10-25 09:18:52 +0300

    Made area have more significant digits

--- visualization.py
+++ visualization.py
@@ -24,5 +24,5 @@ def visualize_counts_plot(data: pd.Series, x_tick_every=100, rolling_avg_size=5,
 def plot_peak_info(peak_channel, peak_height, area, std, mean, area_std, std_std, mean_std, text_height_mul=1.05):
     peak_text = f"$\mu$={mean:.1f}$\pm${mean_std:.1f}\n" \
                 f"$\sigma$={std:.2f}$\pm${std_std:.2f}\n" \
-                f"Area={area:.2f}$\pm${area_std:.2f}"
+                f"Area={area:.3f}$\pm${area_std:.3f}"
     plt.annotate(peak_text, xy=(peak_channel, peak_height), ha='center')
