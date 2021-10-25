d34e93688702b7d65c4a7c3149533950b57c5336
yonatangideoni <yonatangideoni@cs.huji.ac.il>
2021-10-25 23:47:12 +0300

    Added energy calibration graph

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import numpy as np
 from matplotlib import pyplot as plt
+from scipy.optimize import curve_fit
 
 from peak_analysis import find_peaks, fit_gaussian_to_peak
 from read_input import read_counts_file
@@ -6,16 +8,38 @@ from visualization import visualize_counts_plot
 
 if __name__ == '__main__':
     data = read_counts_file("thr45measurement1104.itx")
-    visualize_counts_plot(data)
+
+    # visualize_counts_plot(data)
 
     peaks, _ = find_peaks(data)
     peak_loc = []
     peak_std = []
     for peak in peaks:
-        [peak_area, gaussian_std, peak_channel], cov_mat = fit_gaussian_to_peak(data, peak, plot=True)
+        [peak_area, gaussian_std, peak_channel], cov_mat = fit_gaussian_to_peak(data, peak, plot=False)
         peak_loc.append(peak_channel)
         peak_std.append(cov_mat[-1, -1] ** 0.5)
 
-    plt.xlim(min(peaks) * 0.8)
+    # plt.xlim(min(peaks) * 0.8)
+
+    energies = [5340.36, 5423.15, 5685.37, 6050.78, 6288.08, 6778.3, 8784.86]
+
+    plt.scatter(peak_loc, energies)
+    plt.errorbar(peak_loc, energies, xerr=peak_std, fmt='none', ecolor='r')
+
+    params, cov_mat = curve_fit(lambda x, a, b: (x - b) / a, energies, peak_loc, sigma=peak_loc)
+
+    channels = np.linspace(0, max(peaks))
+    plt.plot(channels, params[0] * channels + params[1], c="k")
+
+    plt.title('Alpha Decay Energy to Channels Fit'
+              f'\nE=({params[0]:.3f}$\pm${cov_mat[0, 0] ** 0.5:.3f})C-{-params[1]:.2f}$\pm${cov_mat[-1, -1] ** 0.5:.2f}')
+
+    print(params, cov_mat)
+
+    plt.xlabel('Channels')
+    plt.ylabel('Energy [KeV]')
+
+    plt.xlim(0)
+    plt.ylim(0)
 
     plt.show()
--- peak_analysis.py
+++ peak_analysis.py
@@ -46,21 +46,4 @@ def fit_gaussian_to_peak(data, peak_channel, delta=8, plot=False):
 
 if __name__ == '__main__':
     pass
-    # energies = [5340.36, 5423.15, 5685.37, 6050.78, 6288.08, 6778.3, 8784.86]
-    #
-    # plt.scatter(peaks, energies)
-    #
-    # params, cov_mat = curve_fit(lambda x, a, b: a * x + b, peaks, energies)
-    #
-    # channels = np.linspace(0, max(peaks))
-    # plt.plot(channels, params[0] * channels + params[1])
-    #
-    # print(params, cov_mat)
-    #
-    # plt.xlabel('Channels')
-    # plt.ylabel('Energy [KeV]')
-    #
-    # plt.xlim(0)
-    # plt.ylim(0)
-    #
-    # plt.show()
+    # energies =
