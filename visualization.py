dd518f8a794232b02e567d4e7e108512240ed17b
yonatangideoni <yonatangideoni@cs.huji.ac.il>
2021-10-30 16:56:55 +0300

    Changed xticks font size

--- visualization.py
+++ visualization.py
@@ -20,7 +20,8 @@ def visualize_counts_plot(data: pd.Series, x_tick_every=100, rolling_avg_size=5,
 
         ax = data.where(data.index.isin(peaks), 0).plot.bar(color='r', ax=ax, label="Peaks", width=1, alpha=alpha)
 
-    ax.set_xticklabels([tick if not i % x_tick_every else "" for i, tick in enumerate(ax.get_xticklabels())])
+    ax.set_xticklabels([tick if not i % x_tick_every else "" for i, tick in enumerate(ax.get_xticklabels())],
+                       fontsize=11)
     plt.xlabel("Channel", fontsize=14)
     plt.ylabel("Density" if normalize else "Counts", fontsize=14)
     plt.legend()
