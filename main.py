5969a5a626c81151ab2a2e49a12ba0cf5f87c11b
roinevo <roi.nevo@mail.huji.ac.il>
2021-10-31 22:15:27 +0200

    add function reading delta t add function files_lst() that calculate the exponential graph

--- main.py
+++ main.py
@@ -1,26 +1,76 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.optimize import curve_fit
-
+import os
 from peak_analysis import find_peaks, fit_gaussian_to_peak
-from read_input import read_counts_file
+from read_input import read_counts_file, read_counts_file_time
 from visualization import visualize_counts_plot
 
-if __name__ == '__main__':
-    visualize_counts_plot(read_counts_file('thr30measurementAl1159.itx'), plot_peaks=False, data_label='With Aluminium')
+def find_max(my_list):
+    max = my_list[0]
+    index = 0
+    for i in range(1, len(my_list)):
+        if my_list[i] > max:
+            max = my_list[i]
+            index = i
+    return max, index
 
-    data = read_counts_file("thr45measurement1104.itx")
+
+def max_point_val(data):
+    peaks, height = find_peaks(data, max_rel_peak_size=3., min_peak_dist=100)
+    #max_peak = max()
+    # max, index = find_max(list(data[10:]))
+    # index = index+10
+    max, temp_index = find_max(height['peak_heights'])
+    index = peaks[temp_index]
+    return max, index
+
+
+def max_point_activity(file_name):
+    delta_t = read_counts_file_time(file_name)
+    data = read_counts_file(file_name)
+    max_peak,_ = max_point_val(data)
+    return max_peak/delta_t, delta_t
+
+def files_lst():
+    # for file in os.listdir():
+    #     # Check whether file is in text format or not
+    #     if file.startswith("thr30unknown"):
+    #         print(read_counts_file_time(file))
+    activities = [max_point_activity(file)[0] for file in os.listdir() if file.startswith("thr30unknown")]
+    delta_ts_temp = [max_point_activity(file)[1] for file in os.listdir() if file.startswith("thr30unknown")]
+    delta_ts = [sum(delta_ts_temp[:i]) for i in range(len(delta_ts_temp))]
+    record_time = [int(file[12:16]) for file in os.listdir() if file.startswith("thr30unknown")]
+    plt.plot(delta_ts, activities,'.')
+    plt.xlabel('delta_t')
+    plt.ylabel('activity of max peak (num/sec')
+    params, cov_mat = curve_fit(lambda x,a,l:a*np.exp(-x/l), np.array(delta_ts)/60, activities,bounds=[0, 50])
+    y = params[0]*np.exp(-np.array(delta_ts)/params[1])
+    plt.plot(delta_ts, y, '.')
+    plt.figure()
+    return activities, record_time
+
+
+
+if __name__ == '__main__':
+    aluminium_data=read_counts_file('thr30measurementAl1159.itx')
+    activity = max_point_activity('thr30measurementAl1159.itx')
+    files_lst()
+    #visualize_counts_plot(aluminium_data, plot_peaks=False, data_label='With Aluminium')
+
+    data = read_counts_file("thr30unknown1157.itx")
 
     visualize_counts_plot(data, alpha=0.4, c='m', plot_peaks=False, data_label='Without Aluminium')
 
-    # peaks, _ = find_peaks(data)
-    # peak_loc = []
-    # peak_std = []
-    # for peak in peaks:
-    #     [peak_area, gaussian_std, peak_channel], cov_mat = fit_gaussian_to_peak(data, peak, plot=False)
-    #     peak_loc.append(peak_channel)
-    #     peak_std.append(cov_mat[-1, -1] ** 0.5)
-    #
+    peaks, _ = find_peaks(data,max_rel_peak_size=3.,min_peak_dist=100)
+    peak_loc = []
+    peak_std = []
+    for peak in peaks:
+        [peak_area, gaussian_std, peak_channel], cov_mat = fit_gaussian_to_peak(data, peak, plot=True,delta=40)
+        peak_loc.append(peak_channel)
+        peak_std.append(cov_mat[-1, -1] ** 0.5)
+
+    #####################
     # # plt.xlim(min(peaks) * 0.8)
     #
     # energies = [5340.36, 5423.15, 5685.37, 6050.78, 6288.08, 6778.3, 8784.86]
--- read_input.py
+++ read_input.py
@@ -1,15 +1,36 @@
 import pandas as pd
+import os
+
+# Import Module
+
+
+# Folder Path
 
 
 def read_counts_file(path: str, max_row=2048) -> pd.Series:
     with open(path, "r") as f:
         raw_data = f.read().split("\n")
     numeric_data = [float(data) for data in raw_data if data.isnumeric()]
-
+    start_time = [float(data[13:24]) for data in raw_data if data.startswith('StartTime')][0]
+    end_time = [float(data[12:23]) for data in raw_data if data.startswith('StopTime')][0]
     data_df = pd.Series(numeric_data)
+
 
     return data_df.iloc[:max_row]
 
 
+def read_counts_file_time(path: str, max_row=2048) -> pd.Series:
+    with open(path, "r") as f:
+        raw_data = f.read().split("\n")
+    start_time = [float(data[13:24]) for data in raw_data if data.startswith('StartTime')][0]
+    end_time = [float(data[12:23]) for data in raw_data if data.startswith('StopTime')][0]
+    delta_t = int(end_time - start_time)
+
+    return delta_t
+
+
 if __name__ == '__main__':
-    df = read_counts_file("histogram.itx")
+    print('h')
+    #df = read_counts_file("thr45measurement1104.itx")
+    #delta_t = read_counts_file_time("thr45measurement1104.itx")
+    #print(delta_t)
