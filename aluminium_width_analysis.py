4c21bdce6f2cdfa17f6917e18e8e3e1e6a3cba0c
roinevo <roi.nevo@mail.huji.ac.il>
2021-11-02 19:35:28 +0200

    Added visualization

--- aluminium_width_analysis.py
+++ aluminium_width_analysis.py
@@ -1,4 +1,19 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
 from read_input import read_counts_file
+from visualization import visualize_counts_plot
+
+
+def rebin_data(data: pd.Series, bin_size: int):
+    rebinned_data = data.groupby(data.index // bin_size).apply(sum)
+
+    return rebinned_data
+
 
 if __name__ == '__main__':
     aluminium_data = read_counts_file('thr30measurementAl1159.itx')
+    rebinned_data = rebin_data(aluminium_data, bin_size=3)
+    visualize_counts_plot(rebinned_data, plot_peaks=False)
+
+    plt.show()
