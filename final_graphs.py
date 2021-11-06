3f8343b244873a92609728387da18da231483a19
yonatangideoni <yonatangideoni@cs.huji.ac.il>
2021-11-06 10:01:58 +0200

    Created energy_spectrum graphing function

--- final_graphs.py
+++ final_graphs.py
@@ -1,12 +1,15 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from scipy.optimize import curve_fit
 
 from peak_analysis import correct_mixed_peaks, find_peaks
 from read_input import read_counts_file
 from visualization import visualize_counts_plot
 
+ENERGIES = np.array([5340.36, 5423.15, 5685.37, 6050.78, 6288.08, 6778.3, 8784.86])
 
-def energy_spectrum():
+
+def counts_spectrum():
     data = read_counts_file('thr10sync1303.itx')
     data.iloc[:10] = 0
 
@@ -17,14 +20,13 @@ def energy_spectrum():
     data.where(data.index.isin(peaks), 0).plot.bar(label='Local Maximum Peaks', color='r', ax=plt.gca(), width=2.)
 
     arrow_dy = data.max() / 15
-    energies = [5340.36, 5423.15, 5685.37, 6288.08, 6050.78, 6778.3, 8784.86]
-    for peak, energy in zip(peaks, energies):
+    for peak, energy in zip(peaks, ENERGIES):
         arrow_start_y = data.iloc[peak] + arrow_dy + 15
         plt.arrow(peak, arrow_start_y, dx=0, dy=-arrow_dy, color='r', width=0.1, head_width=10)
 
         peak_text = f'$E$={energy}[keV]\n' \
                     f'$\mu$={peak}$\pm${1. / 3:.1f}'
-        plt.text(peak, arrow_start_y, peak_text, ha='right' if energy == min(energies) else 'center', fontsize=10)
+        plt.text(peak, arrow_start_y, peak_text, ha='right' if energy == min(ENERGIES) else 'center', fontsize=10)
 
     mixed_peaks = peaks[:2]
     refined_mixed_peaks = correct_mixed_peaks(data, mixed_peaks)
@@ -41,7 +43,32 @@ def energy_spectrum():
     return peaks, [1. / 3] * len(peaks)
 
 
+def energy_spectrum(peaks: list, peak_error: list):
+    plt.figure()
+
+    plt.scatter(ENERGIES, peaks, s=50)
+    plt.errorbar(ENERGIES, peaks, yerr=peak_error, fmt='none', ecolor='r')
+
+    params, cov_mat = curve_fit(lambda x, a, b: a * x + b, ENERGIES, peaks, sigma=peak_error)
+
+    energies = np.linspace(0, max(ENERGIES))
+    plt.plot(energies, params[0] * energies + params[1], c="k")
+
+    plt.title('Alpha Decay Energy to Channels Fit'
+              f'\nC=({params[0]:.3f}$\pm${cov_mat[0, 0] ** 0.5:.3f})E+{params[1]:.2f}$\pm${cov_mat[-1, -1] ** 0.5:.2f}',
+              fontsize=15)
+
+    plt.xlabel('Energy[keV]', fontsize=12)
+    plt.ylabel('Channel', fontsize=12)
+
+    peaks = np.array(peaks)
+    chisq = ((peaks - params[0] * ENERGIES - params[1]) ** 2 / np.array(peak_error) ** 2).sum()
+    print(chisq)
+
+
 if __name__ == '__main__':
-    energy_spectrum()
+    peaks, peak_error = counts_spectrum()
+
+    energy_spectrum(peaks, peak_error)
 
     plt.show()
