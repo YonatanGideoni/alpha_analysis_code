1bf60079e03ec3ef1524254498e43d19a6e3ec80
yonatangideoni <yonatangideoni@cs.huji.ac.il>
2021-11-06 12:05:11 +0200

    General improvements

--- final_graphs.py
+++ final_graphs.py
@@ -9,15 +9,18 @@ from visualization import visualize_counts_plot
 ENERGIES = np.array([5340.36, 5423.15, 5685.37, 6050.78, 6288.08, 6778.3, 8784.86])
 
 
-def counts_spectrum():
+def counts_spectrum(rebin_size=2):
     data = read_counts_file('thr10sync1303.itx')
     data.iloc[:10] = 0
 
+    data = data.groupby(data.index // rebin_size).apply(sum)
+
     visualize_counts_plot(data, plot_peaks=False, data_label='Raw Data', normalize=False)
 
-    peaks, _ = find_peaks(data, max_rel_peak_size=6.)
+    peaks, _ = find_peaks(data, max_rel_peak_size=6., min_peak_dist=15 // rebin_size)
+    print(f'Num peaks: {len(peaks)}')
 
-    data.where(data.index.isin(peaks), 0).plot.bar(label='Local Maximum Peaks', color='r', ax=plt.gca(), width=2.)
+    data.where(data.index.isin(peaks), 0).plot.bar(label='Local Maximum Peaks', color='r', ax=plt.gca(), width=1.)
 
     arrow_dy = data.max() / 15
     for peak, energy in zip(peaks, ENERGIES):
@@ -25,22 +28,24 @@ def counts_spectrum():
         plt.arrow(peak, arrow_start_y, dx=0, dy=-arrow_dy, color='r', width=0.1, head_width=10)
 
         peak_text = f'$E$={energy}[keV]\n' \
-                    f'$\mu$={peak}$\pm${1. / 3:.1f}'
+                    f'$\mu$={peak:.1f}$\pm${1. / 3 * rebin_size:.1f}'
         plt.text(peak, arrow_start_y, peak_text, ha='right' if energy == min(ENERGIES) else 'center', fontsize=10)
 
     mixed_peaks = peaks[:2]
-    refined_mixed_peaks = correct_mixed_peaks(data, mixed_peaks)
-    print(mixed_peaks, refined_mixed_peaks)
+    refined_mixed_peaks = correct_mixed_peaks(data, mixed_peaks, delta=8 // rebin_size)
+    print(f'Original peaks loc: {mixed_peaks}, refined loc: {refined_mixed_peaks}')
 
     plt.legend(fontsize=12)
 
-    plt.xticks(np.arange(0, data.index.max(), 100), fontsize=12)
+    plt.xticks(np.arange(0, data.index.max(), 100 // rebin_size),
+               labels=map(str, np.arange(0, data.index.max() * rebin_size, 100)),
+               fontsize=12)
     plt.yticks(fontsize=12)
-    plt.xlim(1000)
+    plt.xlim(1000 // rebin_size, 2000 // rebin_size)
 
     plt.title('$^{228}$Th Calibration Measurement Counts-per-Channel Spectrum', fontsize=15)
 
-    return peaks, [1. / 3] * len(peaks)
+    return np.array(peaks) * rebin_size, [1. / 3 * rebin_size] * len(peaks)
 
 
 def energy_spectrum(peaks: list, peak_error: list):
@@ -49,20 +54,24 @@ def energy_spectrum(peaks: list, peak_error: list):
     plt.scatter(ENERGIES, peaks, s=50)
     plt.errorbar(ENERGIES, peaks, yerr=peak_error, fmt='none', ecolor='r')
 
-    params, cov_mat = curve_fit(lambda x, a, b: a * x + b, ENERGIES, peaks, sigma=peak_error)
+    params, cov_mat = curve_fit(lambda x, a, b: (x - b) / a, ENERGIES, peaks)
 
     energies = np.linspace(0, max(ENERGIES))
-    plt.plot(energies, params[0] * energies + params[1], c="k")
+    plt.plot(energies, (energies - params[1]) / params[0], c="k")
 
-    plt.title('Alpha Decay Energy to Channels Fit'
-              f'\nC=({params[0]:.3f}$\pm${cov_mat[0, 0] ** 0.5:.3f})E+{params[1]:.2f}$\pm${cov_mat[-1, -1] ** 0.5:.2f}',
+    plt.title('Channels to Alpha Decay Energy Linear Calibration'
+              f'\nC=(E-({params[1]:.2f}$\pm${cov_mat[-1, -1] ** 0.5:.2f}))/'
+              f'({params[0]:.3f}$\pm${cov_mat[0, 0] ** 0.5:.3f})',
               fontsize=15)
 
     plt.xlabel('Energy[keV]', fontsize=12)
     plt.ylabel('Channel', fontsize=12)
 
+    plt.xlim(0)
+    plt.ylim(0)
+
     peaks = np.array(peaks)
-    chisq = ((peaks - params[0] * ENERGIES - params[1]) ** 2 / np.array(peak_error) ** 2).sum()
+    chisq = ((peaks - (ENERGIES - params[1]) / params[0]) ** 2 / np.array(peak_error) ** 2).sum()
     print(chisq)
 
 
