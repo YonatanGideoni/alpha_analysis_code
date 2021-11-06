7b544307b82ceb841572f820773a17d8b9ba8e61
yonatangideoni <yonatangideoni@cs.huji.ac.il>
2021-11-06 12:19:43 +0200

    Improved xticks in visualization

--- visualization.py
+++ visualization.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 
@@ -20,8 +21,8 @@ def visualize_counts_plot(data: pd.Series, x_tick_every=100, rolling_avg_size=5,
 
         ax = data.where(data.index.isin(peaks), 0).plot.bar(color='r', ax=ax, label="Peaks", width=1, alpha=alpha)
 
-    ax.set_xticklabels([tick if not i % x_tick_every else "" for i, tick in enumerate(ax.get_xticklabels())],
-                       fontsize=11)
+    plt.xticks(np.arange(0, data.index.max(), 100))
+
     plt.xlabel("Channel", fontsize=14)
     plt.ylabel("Density" if normalize else "Counts", fontsize=14)
     plt.legend()
