6e0f6ce20cde76adcba7d70555b2f154f1248cb5
yonatangideoni <yonatangideoni@cs.huji.ac.il>
2021-11-08 08:49:21 +0200

    Created fit_gaussian_via_chisq

--- peak_analysis.py
+++ peak_analysis.py
@@ -2,7 +2,7 @@ import numpy as np
 import seaborn as sns
 import pandas as pd
 from matplotlib import pyplot as plt
-from scipy import signal
+from scipy import signal, stats
 from scipy.optimize import curve_fit
 from scipy.stats import linregress
 
@@ -27,13 +27,11 @@ def area_based_double_gaussians(x, I1, s1, u1, I2, u2, s2):
            (I2 / ((2 * np.pi) ** 0.5 * s2)) * np.exp(-(x - u2) ** 2 / (2 * s2 ** 2))
 
 
-def fit_gaussian_to_peak(data, peak_channel, delta=8, plot=False):
-    channels = np.arange(peak_channel - delta, peak_channel + delta + 1)
-    energy_spectrum = data.iloc[peak_channel - delta:peak_channel + delta + 1]
-
+def fit_gaussian_to_peak(energy_spectrum, channels, peak_channel, init_sigma=5, plot=False):
     area_init_guess = energy_spectrum.sum()
     params, cov_mat = curve_fit(area_based_gaussian, channels, energy_spectrum,
-                                p0=[area_init_guess, delta, peak_channel], bounds=[0, np.inf])
+                                p0=[area_init_guess, init_sigma, peak_channel], bounds=[0, np.inf],
+                                sigma=energy_spectrum ** 0.5)
     I, s, u = params
 
     if plot:
@@ -41,13 +39,34 @@ def fit_gaussian_to_peak(data, peak_channel, delta=8, plot=False):
         dense_channels = np.linspace(channels.min(), channels.max())
         plt.plot(dense_channels, area_based_gaussian(dense_channels, I, s, u), linewidth=3, c=colour)
 
-        spread_channels = np.linspace(channels.min() - delta, channels.max() + delta)
+        spread_channels = np.linspace(channels.min() - init_sigma, channels.max() + init_sigma)
         plt.plot(spread_channels, area_based_gaussian(spread_channels, I, s, u), linewidth=2, c=colour,
                  linestyle='dashed')
 
         plot_peak_info(peak_channel, energy_spectrum.max(), I, s, u, *np.diagonal(cov_mat) ** 0.5)
 
     return params, cov_mat
+
+
+def fit_gaussian_via_chisq(data, peak_channel, right_delta=15, left_delta=None, plot=False):
+    if left_delta is not None:
+        energy_spectrum = data.iloc[peak_channel - left_delta:peak_channel + right_delta + 1]
+        channels = np.arange(peak_channel - left_delta, peak_channel + right_delta + 1)
+        return fit_gaussian_to_peak(energy_spectrum, channels, peak_channel, plot=plot)
+
+    deltas = np.arange(1, right_delta)
+    p_val = np.zeros_like(deltas)
+    for i, delta in enumerate(deltas):
+        energy_spectrum = data.iloc[peak_channel - delta:peak_channel + right_delta + 1]
+        channels = np.arange(peak_channel - delta, peak_channel + right_delta + 1)
+        params, cov_mat = fit_gaussian_to_peak(energy_spectrum, channels, peak_channel, plot=plot)
+
+        fitted_gaussian_data = area_based_gaussian(channels, *params)
+        chi_sq = ((fitted_gaussian_data - energy_spectrum) ** 2 / energy_spectrum).sum()
+        dof = len(energy_spectrum) - len(params)
+        p_val[i] = stats.chi2.cdf(chi_sq, dof)
+
+        print(f'Left delta={left_delta}=>chi^2={chi_sq:.2f}, p={p_val[i]:.2f}')
 
 
 if __name__ == '__main__':
